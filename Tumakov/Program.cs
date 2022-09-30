d485befdc51504b3b0835587653c03b0a3053ce4
Makarova Darya <makarovadarya2704@gmail.com>
2022-09-30 15:02:44 +0300

    Add project files.

---
+++ Tumakov/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Tumakov
+{
+    public enum DayoftheWeek
+    {
+        Monday, Tuesday, Wednesday, thursday, Friday, Saturday, Sunday
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+        }
+    }
+}
